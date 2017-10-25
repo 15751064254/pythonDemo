3059819e1a1a7449d70c33e23889a9c5e29c6ce3
1031353632 <1031353632@qq.com>
2017-10-25 18:10:12 +0800

    2_04_microbial-genetic-algorithm.py

---
+++ geneticAlgorithm/2_04_microbial-genetic-algorithm.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+DNA_SIZE = 10   # DNA length
+POP_SIZE = 20   # population size
+CROSS_RATE = 0.6    # mating probability (DNA crossover)
+MUTATION_RATE = 0.6     # mutation probability
+N_GENERATIONS = 200
+X_BOUND = [0, 5]    # x upper and lower bounds
+
+def F(x):
+    return
