3a1a6fd8a0df8203a96d02d4bf1b3e737189374f
1031353632 <1031353632@qq.com>
2017-12-11 11:33:43 +0800

    robot

--- stocks/env.py
+++ stocks/env.py
@@ -2,14 +2,30 @@ import numpy as np
 import pyglet
 
 class Env(object):
-    biewer = None
+    viewer = None
+    dt = .1
+    action_bound = [-1, 1]
+    goal = {'x': 100., 'y': 100., 'l': 40}
+    state_dim = 9
+    action_dim = 2
+    target = 1000
 
     def __init__(self):
+        self.arm_info = np.zeros(2, dtype=[('l', np.float32), ('r', np.float32)])
+        self.arm_info['l'] = 100
+        self.arm_info['r'] = 200
+        self.on_goal = 0
 
     def step(self, action):
         done = False
+        r = 2
+        s = 1
+        return s, r, done
 
     def reset(self):
+        s = np.concatenate()
+        return s
+
 
     def render(self):
         if self.viewer is None:
@@ -28,6 +44,9 @@ class Viewer(pyglet.window.Window):
         # vsync = False to not use the monitor FPS, we can speed up training
         super(Viewer, self).__init__(width=400, height=400, resizable=False, caption='Env', vsync=False)
         pyglet.gl.glClearColor(1, 1, 1, 1)
+        self.batch = pyglet.graphics.Batch()
+
+        self.arm_info = arm_info
 
 
     def render(self):
@@ -43,6 +62,8 @@ class Viewer(pyglet.window.Window):
         self.batch.draw()
 
     def _update_arm(self):
+        return 0
+
 
 
 
@@ -50,4 +71,4 @@ if __name__ == '__main__':
     env = Env()
     while True:
         env.render()
-        env.step(env.sample_action())
+        #env.step(env.sample_action())
