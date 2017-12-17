25b55ed0632238d0ec1b20b91e223715d11a4327
15751064254 <l15751064254@gmail.com>
2017-12-18 01:06:26 +0800

    mnist

--- mnist/mnist_softmax.py
+++ mnist/mnist_softmax.py
@@ -30,7 +30,7 @@ def main(_):
     # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
 
-    # the raw formulation of cross-entropy,
+    # The raw formulation of cross-entropy,
     # tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
     #                                reduction_indices=[1]))
     # can be numerically unstable.
@@ -44,7 +44,7 @@ def main(_):
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
     sess = tf.InteractiveSession()
-    tf.global_variable_initializer().run()
+    tf.global_variables_initializer().run()
 
     # Train
     for _ in range(1000):
@@ -55,13 +55,16 @@ def main(_):
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                        y_: muist.test.labels}))
+                                        y_: mnist.test.labels}))
 
 
 if __name__ == '__main__':
-    parse = argparse.ArgumentParser()
-    parse.add_argument('--data_dir', type=str,
-            default='/tmp/tensorflow/mnist/input_data',
-            help='Directory for storing input data')
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--data_dir', 
+        type=str,
+        default='./tensorflow/mnist/input_data',
+        help='Directory for storing input data'
+    )
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
